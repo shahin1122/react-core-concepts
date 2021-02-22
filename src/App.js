4254e8c1714c86d98fffd1e56d727f26abf6d704
shahin1122 <passportscotch7@gmail.com>
2021-02-23 02:07:05 +0600

    react product

--- src/App.js
+++ src/App.js
@@ -2,31 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const products = [
+    {name: 'photoshop' , price : '49.99$'} ,
+    {name: 'Illstrator' , price : '99.99$'},
+    {name: 'PDF reader' , price : '11.99$'},
+  ]
+  
+
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a react person</p>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
-      </header>
+
+        <Product costume={products[0]}></Product>
+        <Product costume={products[1]}></Product>
+        <Product costume={products[2]}></Product>
+
+        </header>
     </div>
   );
 }
 
-function Person() {
-  const personStyle = {
-    border : '8px solid forestGreen',
-    margin : '10px',
-    padding:'10px',
-    borderRadius : '10px',
+function Product(props) {
+  const productStyle = {
+      border : '1px solid gray',
+      borderRadius : '5px',
+      backgroundColor: 'lightGray',
+      height: '400px',
+      width : '400px',
+      float : 'left',
+      color: 'tomato',
+      margin: '20px'
+     
   }
+
+  // const {name , price} = props.products ;
+  // console.log(name  , price);
+
+  const {name , price} = props.costume;
+  console.log(name, price);
+
+  
   return (
-    <div style = {personStyle}> 
-        <h1>Name: Shakib</h1>
-        <h3>Hero of the year</h3>
-    </div>
+      <div style={productStyle}>
+          <h3>Name: {name} </h3>
+          <h2>Price: {price} </h2>
+          <button> Buy Now: </button>
+
+      </div>
   )
 }
 
