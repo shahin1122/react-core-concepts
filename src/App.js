ffd78520d4f61bdde541b8c976cef69668f91559
shahin1122 <passportscotch7@gmail.com>
2021-02-23 00:34:18 +0600

    updated component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        
-       
+        <p>I am a react person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
+
+function Person() {
+  const personStyle = {
+    border : '8px solid forestGreen',
+    margin : '10px',
+    padding:'10px',
+    borderRadius : '10px',
+  }
+  return (
+    <div style = {personStyle}> 
+        <h1>Name: Shakib</h1>
+        <h3>Hero of the year</h3>
+    </div>
+  )
+}
+
 
 export default App;
